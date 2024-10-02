fbf5689b422ec76cbcb2610068321baf809c35dd
heyoungan <159891097+heyoungan@users.noreply.github.com>
2024-10-02 18:30:42 +0900

    Title: 다양하게 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241002/다양하게 출력/print-in-variety.java
+++ 241002/다양하게 출력/print-in-variety.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void (main(String[] args) {
-       System.out.print("Total days in Year\n265\nCircumference rate\n3.1415926535");
+       System.out.print("Total days in Year\n365\nCircumference rate\n3.1415926535");
     }
 }
